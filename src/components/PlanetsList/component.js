84bc1ac8ae7e3bdd9ce9af5aa59057cfc358371c
Kurone36 <58810589+Kurone36@users.noreply.github.com>
2021-06-26 15:53:37 +0200

    Update component.js

--- src/components/PlanetsList/component.js
+++ src/components/PlanetsList/component.js
@@ -5,12 +5,16 @@ export default {
   name: 'PlanetsList',
   data () {
     return {
-      lists: []
+      lists: [],
+      isLoading: true
     }
   },
   mounted () {
     axios
       .get('https://swapi.dev/api/planets/')
       .then(response => (this.lists = response.data.results))
+    setTimeout(() => {
+      this.isLoading = false
+    }, 3000)
   }
 }
