03010928a49ff58dffb46b3fe2762c27ec104e3c
Kurone36 <58810589+Kurone36@users.noreply.github.com>
2021-06-28 07:45:30 +0200

    Update index.vue

--- src/components/PlanetsList/index.vue
+++ src/components/PlanetsList/index.vue
@@ -1,16 +1,27 @@
 <template>
-    <div id="loading" v-if="isLoading" v-show="loading">Loading</div>
-    <div class="PlanetsList" v-else>
-      <h3>Planet List</h3>
-      <ul class="list" v-for="list in lists" :key="list">
-        <li>Name: {{ list.name }}</li>
-        <li>Rotation period: {{ list.rotation_period }}</li>
-        <li>Climate: {{ list.climate }}</li>
-        <li>Gravity: {{ list.gravity }}</li>
-        <li>Created: {{ list.created }}</li>
-        <li>URL: {{ list.url }}</li>
-      </ul>
-    </div>
+  <div id="loading" v-if="isLoading">Loading</div>
+  <div class="PlanetsList" v-else>
+    <h3>Planet List</h3>
+        <template>
+          <ul class="list" v-for="list in lists" :key="list">
+            <li>Name: {{ list.name }}</li>
+            <li>Rotation period: {{ list.rotation_period }}</li>
+            <li>Climate: {{ list.climate }}</li>
+            <li>Gravity: {{ list.gravity }}</li>
+            <li>Created: {{ list.created }}</li>
+            <li>URL: {{ list.url }}</li>
+          </ul>
+          <div class="pagination-row" >
+            <vspagination>
+              <button class="pagination-button">Previous</button>
+              <span v-for="(list, index) in new Array(10)" :key="index">
+                <button class="pagination-button">{{index + 1}}</button>
+              </span>
+              <button class="pagination-button">Next</button>
+            </vspagination>
+          </div>
+        </template>
+  </div>
 </template>
 
 <style src="./style.sass" lang="sass" scoped></style>
