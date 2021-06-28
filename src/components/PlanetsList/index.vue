fbcaf96c90c393452e950838b4d32519d68b5ee7
Kurone36 <58810589+Kurone36@users.noreply.github.com>
2021-06-28 09:13:04 +0200

    Update index.vue

--- src/components/PlanetsList/index.vue
+++ src/components/PlanetsList/index.vue
@@ -2,27 +2,27 @@
   <div id="loading" v-if="isLoading">Loading</div>
   <div class="PlanetsList" v-else>
     <h3>Planet List</h3>
-        <template>
-          <ul class="list" v-for="list in lists" :key="list">
-            <li>Name: {{ list.name }}</li>
-            <li>Rotation period: {{ list.rotation_period }}</li>
-            <li>Climate: {{ list.climate }}</li>
-            <li>Gravity: {{ list.gravity }}</li>
-            <li>Created: {{ list.created }}</li>
-            <li>URL: {{ list.url }}</li>
-          </ul>
-          <div class="pagination-row" >
-            <vspagination>
-              <button class="pagination-button">Previous</button>
+      <ul class="list" v-for="list in lists" :key="list">
+        <li>Name: {{ list.name }}</li>
+        <li>Rotation period: {{ list.rotation_period }}</li>
+        <li>Climate: {{ list.climate }}</li>
+        <li>Gravity: {{ list.gravity }}</li>
+        <li>Created: {{ list.created }}</li>
+        <li>URL: {{ list.url }}</li>
+      </ul>
+      <template>
+        <vs-pagination v-model="currentPage" @input="pageNumber">
+          <div class="pagination-row">
+              <button class="pagination-button" @click="pageNumber -= 1 ">Previous</button>
               <span v-for="(list, index) in new Array(10)" :key="index">
-                <button class="pagination-button">{{index + 1}}</button>
+                  <button class="pagination-button" @click="pageNumber = index + 1 ">{{index + 1}}</button>
               </span>
-              <button class="pagination-button">Next</button>
-            </vspagination>
-          </div>
-        </template>
+              <button class="pagination-button" @click="pageNumber += 1 ">Next</button>
+            </div>
+        </vs-pagination>
+      </template>
   </div>
 </template>
 
 <style src="./style.sass" lang="sass" scoped></style>
-<script src="./component.js" />
+<script src= "./component.js"></script>
