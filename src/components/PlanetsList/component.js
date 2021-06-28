68ba16bdc039454175311f04ec05caafdf99d196
Kurone36 <58810589+Kurone36@users.noreply.github.com>
2021-06-28 07:45:11 +0200

    Update component.js

--- src/components/PlanetsList/component.js
+++ src/components/PlanetsList/component.js
@@ -1,20 +1,29 @@
 // @vue/component
 import axios from 'axios'
+import VsPagination from 'vs-pagination'
+
+const pageNumber = 2
 
 export default {
   name: 'PlanetsList',
+  components: {
+    VsPagination
+  },
   data () {
     return {
       lists: [],
+      pageNumber: 1,
+      page: 1,
       isLoading: true
     }
   },
   mounted () {
     axios
-      .get('https://swapi.dev/api/planets/')
+      .get(`https://swapi.dev/api/planets/?page=${pageNumber}`)
       .then(response => (this.lists = response.data.results))
+      .then(console.log(this.lists.sort()))
     setTimeout(() => {
       this.isLoading = false
-    }, 3000)
+    }, 2000)
   }
 }
